8476170b52d71b84fb79f51e5fb1267c3fd37617
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2023-05-13 17:03:22 +0530

    window box added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow = WindowSettings::new("PESMario!", [640, 480])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |context, graphics, _device| {
+            clear([1.0; 4], graphics);
+            rectangle([0.0, 0.0, 0.0, 0.0],
+                      [0.0, 0.0, 100.0, 100.0],
+                      context.transform,
+                      graphics);
+        });
+    }
 }
+
+
